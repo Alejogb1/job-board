73c1bd3082c51e0215a38bdd125e492fb96d5a4a
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2024-01-07 22:04:34 -0300

    Update page.tsx

--- app/(default)/company/[name]/page.tsx
+++ app/(default)/company/[name]/page.tsx
@@ -4,18 +4,18 @@ import Sidebar from '@/components/sidebar'
 export default function CompanyProfile() {
   return (
         <div className="content lg:max-w-6xl mx-auto my-0 lg:pt-20">
-            <div className="styles-box">
-                <div className="ml-2 lg:ml-6">
-                    <div className="space-x-1 text-dark-a flex flex-row items-center">
-                        <a className="text-xs" href="/discover">Discover</a>
+            <div className="styles-box mx-6">
+                <div className="lg:pt-1 pt-20">
+                    <div className="space-x-1 text-dark-a flex flex-row items-center lg:pl-6 text-base lg:text-xs">
+                        <a className="" href="/discover">Discover</a>
                         <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className="w-2"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 21a.997.997 0 00.707-.293l8-8a.999.999 0 000-1.414l-8-8a.999.999 0 10-1.414 1.414L14.586 12l-7.293 7.293A.999.999 0 008 21z" fill="currentColor"></path></svg>
-                        <a className="text-xs" href="/discover/companies">Company</a>
+                        <a className="" href="/discover/companies">Company</a>
                         <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className="w-2"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 21a.997.997 0 00.707-.293l8-8a.999.999 0 000-1.414l-8-8a.999.999 0 10-1.414 1.414L14.586 12l-7.293 7.293A.999.999 0 008 21z" fill="currentColor"></path></svg>
-                        <a className="text-xs" href="/company/thumbtack">Thumbtack</a>
+                        <a className="" href="/company/thumbtack">Thumbtack</a>
                     </div>
                 </div>
                 <div className="styles-content">
-                    <section className="max-w-6xl mx-auto sm:px-6 mt-10 max-h-20">
+                    <section className="max-w-6xl mx-auto sm:px-6 mt-10 lg:mt-10 max-h-20">
                         <div className="max-h-8 flex flex-row items-center">
                             <div className="logo mr-3">
                                 <a className="">
@@ -25,7 +25,7 @@ export default function CompanyProfile() {
                                 </a>
                             </div>
                             <div className="name">
-                                <div className="name flex flex-row space-between max-h-8">
+                                <div className="name flex flex-row space-between max-h-6 lg:max-h-8">
                                     <h3 className="text-xl font-semibold font-inter mb-10">Thumbstack</h3>
                                 </div>
                                 <h2 className=''>A technology company helping millions of people care for and improve their homes</h2>
@@ -35,26 +35,11 @@ export default function CompanyProfile() {
                     <div className="styles-main flex flex-row space-between items-start">
                         <div className="w-full">
                             <div className="component-header pt-10">
-                                <div className="mx-8 pt-2 border-b px-2 ">
+                                <div className="lg:mx-8 lg:pt-2 border-b lg:px-2">
                                     <nav className='m-0 flex align-end justify-start flex-row min-w-0 min-h-unset w-full pt-unset'>
-                                        <a href="" className='leading-5 pb-1'>
+                                        <a href="" className='lg:leading-5 pb-1'>
                                             <div className="flex flex-row items-center text-xs">
                                                 <span>Overview</span>
-                                            </div>
-                                        </a>
-                                        <a href="" className='leading-5 pb-1 ml-8'>
-                                            <div className="flex flex-row items-center text-xs">
-                                                <span>People</span>
-                                            </div>
-                                        </a>
-                                        <a href="" className='leading-5 pb-1 ml-8'>
-                                            <div className="flex flex-row items-center text-xs">
-                                                <span>Culture and benefits</span>
-                                            </div>
-                                        </a>
-                                        <a href="" className='leading-5 pb-1 ml-8'>
-                                            <div className="flex flex-row items-center text-xs">
-                                                <span>Funding</span>
                                             </div>
                                         </a>
                                         <a href="" className='leading-5 pb-2 ml-8 flex flex-row border-b-2 border-gray-500 font-semibold'>
@@ -71,15 +56,15 @@ export default function CompanyProfile() {
                                 </div>
                             </div>
                             <div className="profile flex flex-row align-start w-full">
-                                <section className='lg:py-6 lg:px-8'>
-                                    <h1 className='text-xl font-medium text-dark-aaaa antialiased'>Jobs at Thumbstack</h1>
+                                <section className='lg:py-6 lg:px-8 py-8'>
+                                    <h1 className='sm:text-2xl lg:text-xl font-semibold lg:font-medium text-dark-aaaa antialiased'>Jobs at Thumbstack</h1>
                                     <div className="mb-6">
                                         <div className="text-xs">
                                         Thumbtack empowers people to DO. We empower customers to get more done and small business owners to do the work they love.
                                         </div>
                                     </div>
                                     <div className="styles-browser flex">
-                                        <div className="filter md:w-42 lg:w-72">
+                                        <div className="filter md:w-42 lg:w-72 hidden md:block lg:block">
                                             <aside className="md:shrink-0 md:order-1">
                                                 <div data-sticky="" data-margin-top="32" data-sticky-for="768" data-sticky-wrap="">
                                                     <div className="relative bg-gray-50 rounded-sm border border-gray-200 p-5">
