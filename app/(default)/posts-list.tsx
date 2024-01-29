3db9ebe11c481bb73eb4e3dc3712de01b6b64592
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2024-01-29 12:19:55 -0300

    Update posts-list.tsx

--- app/(default)/posts-list.tsx
+++ app/(default)/posts-list.tsx
@@ -1,16 +1,10 @@
 {/*
 Note: This code includes an example of how to fetch data from an external JSON file that is hosted at https://raw.githubusercontent.com/cruip/cruip-dummy/main/job-board-posts.json. To facilitate this, we've included a lib directory in the root which contains a function that can fetch the JSON content. Additionally, we've defined the Post types in the types.d.ts file located in the root.
 */}
-
-import getFilteredPosts from '@/lib/getAllPosts'
 import PostItem from './post-item'
 import Newsletter from '@/components/newsletter'
-import getCompanies from '@/lib/getCompany'
-import Link from 'next/link'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import Pagination from '@/components/ui/pagination'
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import getFilteredPosts from '@/lib/getFilteredPosts'
 
 interface Post {
     id: number,
@@ -23,11 +17,6 @@ interface Post {
       slug: string,
       job_post_url: string,
       created_at: Date,
-  }
-
-  interface PaginationProps {
-    totalPages: number
-    currentPage: number
   }
 
 export default async function PostsList({
