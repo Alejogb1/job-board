f15d161c5ca7515208616249d4f69bec99894282
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2024-02-14 20:51:25 -0300

    search and location parameters

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -7,10 +7,10 @@ export default function SearchBar(){
     const pathname = usePathname();
     const { replace } = useRouter();
 
-    const handleSearch = useDebouncedCallback((term) => {
-        console.log(`Searching... ${term}`);
-       
+    const handleTermSearch = useDebouncedCallback((term) => {
+
         const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
         if (term) {
           params.set('query', term);
         } else {
@@ -18,28 +18,42 @@ export default function SearchBar(){
         }
         replace(`${pathname}?${params.toString()}`);
       }, 300);
-      
+    const handleLocationSearch = useDebouncedCallback((term) => {     
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+
+      if (term) {
+        params.set('loc', term);
+      } else {
+        params.delete('loc');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 300);
     
     return(
-        <form className="styles-form-component sticky top-0 z-0 bg-white w-full lg:pb-2 border-gray-200 lg:mb-4">
+        <form className="styles-form-component sticky pt-4 top-0 z-10 bg-white w-full lg:pb-2 border-gray-200 lg:mb-4">
             <div className="styles-filter lg:p-0">
             <div className="search-bar">
                 <div className="styles">
                 <div className="gap-2 flex flex-col lg:flex-row">
                     <div className="role-wrapper grow lg:basis-6/12 max-w-full">
                         <div className="rounded-sm border border-gray-300 bg-slate-50 text-gray-400 text-base h-10 w-full flex flex-center">
-                                <input   defaultValue={searchParams.get('query')?.toString()}
-
-                                        onChange={(e) => {
-                                            handleSearch(e.target.value);
-                                          }}
-                                  
-                                type="text" placeholder="Search for job title or keyword" className={`text-md rounded-sm whitespace-nowrap boverflow-hidden outline-none border-0 active:outline-none focus:outline-none bg-slate-50 text-gray-800 text-base h-full cursor-text w-full`}                            />                        
+                                <input   
+                                  defaultValue={searchParams.get('query')?.toString()}       
+                                  onChange={(e) => {handleTermSearch(e.target.value);}}
+                                  type="text" placeholder="Search for job title or keyword" className={`text-md rounded-sm whitespace-nowrap boverflow-hidden outline-none border-0 active:outline-none focus:outline-none bg-slate-50 text-gray-800 text-base h-full cursor-text w-full`}                            
+                                />                        
                             </div>
                     </div>
                     <div className="location-wrapper grow basis-6/12 max-w-full">
                         <div className="rounded-sm border border-gray-300 bg-slate-50 text-gray-400 text-base h-10 cursor-pointer w-full" >
-                            <input type="text" placeholder="City or region" className={`text-md rounded-sm whitespace-nowrap boverflow-hidden outline-none border-0 active:outline-none focus:outline-none bg-slate-50 text-gray-800 text-base h-full cursor-text w-full`}                            />
+                            <input 
+                              defaultValue={searchParams.get('loc')?.toString()}       
+                              onChange={(e) => {handleLocationSearch(e.target.value);}}
+                              type="text" 
+                              placeholder="City or region" 
+                              className={`text-md rounded-sm whitespace-nowrap boverflow-hidden outline-none border-0 active:outline-none focus:outline-none bg-slate-50 text-gray-800 text-base h-full cursor-text w-full`}                            
+                            />
                         </div>
                     </div>
                 </div>
