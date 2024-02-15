1ec0d954e0845df8b6b152e7c638364e691b9037
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2024-02-15 09:33:51 -0300

    term and location based database retrieve

--- lib/getFilteredPosts.tsx
+++ lib/getFilteredPosts.tsx
@@ -1,17 +1,54 @@
 'use server'
 import prisma from "./prisma"
 
-export default async function getFilteredPosts(page: number) {
+export default async function getFilteredPosts(page: number, term:string, location:string) {
     const itemsPerPage = 10;
     const skip = (page - 1) * itemsPerPage;
-
     try {
-        const posts: any[] = await prisma.jobPost.findMany({
-            take: itemsPerPage,
-            skip: skip,
-        });
-
-        return posts;
+        if (term && location) {
+            const posts: any[] = await prisma.jobPost.findMany({
+                take: itemsPerPage,
+                skip: skip,
+                where: {
+                    job_title: {
+                        search: term
+                    },
+                    region: {
+                        search: location
+                    }
+                }
+            });
+            return posts;
+        } else if (term) {
+            const posts: any[] = await prisma.jobPost.findMany({
+                take: itemsPerPage,
+                skip: skip,
+                where: {
+                    job_title: {
+                        search: term
+                    }
+                }
+            });
+            return posts;
+        } else if (location) {
+            const posts: any[] = await prisma.jobPost.findMany({
+                take: itemsPerPage,
+                skip: skip,
+                where: {
+                    region: {
+                        search: location
+                    }
+                }
+            });
+            return posts;
+        }
+        else {
+            const posts: any[] = await prisma.jobPost.findMany({
+                take: itemsPerPage,
+                skip: skip,
+            });
+            return posts;
+        }
     } catch (error) {
         console.error('Database Error:', error);
         throw new Error('Failed to fetch job posts.');
