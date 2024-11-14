ccd36fed3c64b4e0b1615964583ab17e1136c4b6
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2024-11-14 11:02:47 -0300

    Update getAllPosts.tsx

--- lib/getAllPosts.tsx
+++ lib/getAllPosts.tsx
@@ -2,6 +2,25 @@
 
 import prisma from './prisma';
 
+export async function getAllJobPostSlugs() {
+    try {
+        // Fetch only the fields needed for the sitemap
+        const jobs = await prisma.jobPost.findMany({
+            select: {
+                slug: true, // Adjust to the correct field name
+            },
+        });
+
+        // Map jobs to a structure needed for the sitemap
+        return jobs.map((job) => ({
+            slug: job.slug,
+            url: `https://www.jobseekr.ai/posts/${job.slug}`,
+        }));
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch job post slugs.');
+    }
+}
 export async function getAllPosts() {
     try {
         // Fetch only the fields needed for the sitemap
